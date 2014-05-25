34bcb32c93b30b40fd8c6e581723ff5d20b24ec0
hgdebarba <hgdebarba@gmail.com>
2014-05-26 00:44:23 +0200

    script ready, readme.md halfway done

---
+++ README.md
@@ -0,0 +1,19 @@
+## 
+### The script run_analysis.R load the sparse movement sensor data available at https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip to produce a tidy datafile as output
+
+The dataset is composed by accelerometer and gyroscope of a Samsung Galaxy II while performing 6 common physical activities.
+The data was originaly split in training and test datasets. Feature names, subject code, activity code and activity code/name correspondence were also available in different files. 
+Additional information on the dataset may be found at http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+The main steps of the script are
+1. load sparse data (training, test, features, subjects, activities, activities name)
+2. merge training and test data
+3. set correct feature name
+3. subset mean and standard deviation data from the dataset
+4. reshape column names to become more readible
+5. add activity and subject codes to the data
+6. switch activity code to activity name
+7. compute mean of data (original mean and SD) by subject code and activity name
+8. generate the tidy data file as a .csv
+
+The tidy data uploaded to coursera was manually converted to .txt (the only text format accepted in the submission page) and may cause readibility issues if not converted back to .csv
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+## read data and set meaningful column names for files with two or less columns
 x_train = read.table("UCI-HAR-Dataset//train//x_train.txt")
 train_subjectID = read.table("UCI-HAR-Dataset//train//subject_train.txt", col.names = "subjectID")
 train_activityID = read.table("UCI-HAR-Dataset//train//y_train.txt", col.names = "activityID")
@@ -6,32 +7,76 @@ x_test = read.table("UCI-HAR-Dataset//test//x_test.txt")
 test_subjectID = read.table("UCI-HAR-Dataset//test//subject_test.txt", col.names = "subjectID")
 test_activityID = read.table("UCI-HAR-Dataset//test//y_test.txt", col.names = "activityID")
 
-activity_labels = read.table("UCI-HAR-Dataset//activity_labels.txt", col.names = c("activityID", "activityName"))
+activityLabels = read.table("UCI-HAR-Dataset//activity_labels.txt", col.names = c("activityID", "activityName"))
 
 featuresList = read.table("UCI-HAR-Dataset//features.txt", stringsAsFactors = F, col.names = c("featureID", "featureName"))
 
-## merge data
+
+
+## 1. Merges the training and the test sets to create one data set.
+## merge training and test data
 mergedData <- rbind(x_train, x_test)
-#mergedData$activityID <- rbind(train_activityID, test_activityID)
-#mergedData$subjectID <- rbind(train_subjectID, test_subjectID)
 
 
-## get feature mean/std name and index
-mean_std_index = rbind(featuresList[grep("-mean\\(\\)", featuresList$featureName) ,],featuresList[grep("std\\(\\)", featuresList$featureName) ,])
-colnames(mean_std_index) <- c("ID", "featureName")
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+## select >name< and >index< of >mean< and >SD< features using partial match of the strings "mean()" and "std()" 
+featureMeanSD = rbind(featuresList[grep("mean\\(\\)", featuresList$featureName) ,],featuresList[grep("std\\(\\)", featuresList$featureName) ,])
 
-## mean std data
-meanStdData <- mergedData[,order(mean_std_index$ID)]
-colnames(meanStdData) <- mean_std_index$featureName
+## set meaningful column names for featureMeanSD
+colnames(featureMeanSD) <- c("ID", "featureName")
 
-meanStdData$activityID <- c(train_activityID$activityID, test_activityID$activityID)
-meanStdData$subjectID <- c(train_subjectID$subjectID, test_subjectID$subjectID)
+## subset mean and SD data
+meanSDData <- mergedData[,featureMeanSD$ID]
 
-meanStdData = merge(meanStdData, activity_labels)#, by.x = "activityID", by.y = "activityID")
-head(meanStdData, n=3)
-tapply(meanStdData[,5], list(meanStdData$subjectID, meanStdData$activityName), mean)
 
-install.packages("data.table")
-library(data.table)
-meanStdDT <- as.data.table(meanStdData)
-newDT <- meanStdDT[, mean(colnames(meanStdDT)),   by="subjectID,activityName"]
+
+## 4. Appropriately labels the data set with descriptive activity names. 
+## mean() and std() to Mean and SD
+featureMeanSD$featureName = gsub("mean\\(\\)", "Mean",featureMeanSD$featureName)
+featureMeanSD$featureName = gsub("std\\(\\)", "SD",featureMeanSD$featureName)
+## f and t to FFT (fast fourrier transform) and Time
+featureMeanSD$featureName = gsub("fB", "FFT\\-B",featureMeanSD$featureName)
+featureMeanSD$featureName = gsub("tB", "Time\\-B",featureMeanSD$featureName)
+featureMeanSD$featureName = gsub("tG", "Time\\-G",featureMeanSD$featureName)
+featureMeanSD$featureName = gsub("\\-", "_",featureMeanSD$featureName)
+
+
+## use feature names to set meaningful data names
+colnames(meanSDData) <- featureMeanSD$featureName
+
+
+
+## 3. Uses descriptive activity names to name the activities in the data set
+## add two more columns to meanSDData, the activityID and subjectID
+meanSDData$activityID <- c(train_activityID$activityID, test_activityID$activityID)
+meanSDData$subjectID <- c(train_subjectID$subjectID, test_subjectID$subjectID)
+
+## merge meanSDData and activityLabels, they share the factor named activityID
+## as a result the column activityLabels$activityName is added to meanSDData
+meanSDData = merge(meanSDData, activityLabels)
+meanSDData$activityID <- NULL
+write.csv(file="mean_SD_data.csv", x=meanSDData, row.names = F)
+
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+## compute mean of means and SD
+
+meanOfMeans = NULL;
+
+## initialize the tidyData data frame with subjectID and activityName. 
+## Third column (the actual goal of aggregate) was dropped as the code in the for loop
+## can compute it for all columns while preserving the column name
+tidyData = aggregate(meanSDData[[featureMeanSD$featureName[1]]] ~ subjectID + activityName, 
+                      data = meanSDData, FUN = mean)[,1:2]
+
+## for all relevant features (mean and SD)
+for (i in 1:length(featureMeanSD$featureName)){
+    newName = featureMeanSD$featureName[i]
+    ## mean for this column for every combination of subjectID and activityName
+    meanOfMeans = aggregate(meanSDData[[newName]] ~ subjectID + activityName, data = meanSDData, FUN = mean)
+    ## add new column to tidyData with the correct name
+    newName = paste("Mean_of_", newName)
+    tidyData[,newName] = meanOfMeans[,"meanSDData[[newName]]"]
+}
+## write the data into a CSV file
+write.csv(file="Mean_of_means_and_SDs.csv", x=tidyData, row.names = F)
+
